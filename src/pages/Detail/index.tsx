5ac284fc800cbd9d8ace34a041bcf8c319077b4d
Maurício <72310791+mauricio8989@users.noreply.github.com>
2025-02-21 12:25:29 +0000

    :recycle: removing file with problem

--- src/pages/Detail/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Project } from "../../components/Project"
-import { projects } from "../Home"
-import { useParams } from 'react-router-dom';
-
-export function Detail(){
-    const { id } = useParams()
-    
-}
